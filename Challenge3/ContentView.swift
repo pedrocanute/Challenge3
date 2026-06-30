0780e547fa52fc7c1a5f86835986efb262e59147
Pedro Canute <pedro.canute2@gmail.com>
2026-06-30 11:16:58 -0300

    Initial Commit

---
+++ Challenge3/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import Playgrounds
+
+@main struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
